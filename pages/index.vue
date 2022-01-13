1b719af114ae6be1a653a8f5b21f79a60be2bd88
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2022-01-13 16:02:02 +0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <canvas ref="canvas" width="1024" height="512" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import gol from '~/includes/gol/gol.js'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  mounted () {
+    gol(this.$refs.canvas as HTMLCanvasElement)
+  }
+})
+</script>
