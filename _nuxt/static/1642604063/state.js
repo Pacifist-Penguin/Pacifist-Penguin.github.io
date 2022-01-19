e73ad97c376766f061767227d09ef128a7c60ea6
NewPirateOfUASeas <NewPirateOfUASeas@users.noreply.github.com>
2022-01-19 14:54:24 +0000

    deploy: 9275dc476c2d7e5aa098a2517f2dd617c23d9be1

---
+++ _nuxt/static/1642604063/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642604063",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"40bf2b0b"}}}}(null,"\u002F"));
