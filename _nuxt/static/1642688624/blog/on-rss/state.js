7da9282104c27fea56fca48131ac95971e04be24
NewPirateOfUASeas <NewPirateOfUASeas@users.noreply.github.com>
2022-01-20 14:23:45 +0000

    deploy: d4f79858cf02886b42d91186170e530ad12e0adc

---
+++ _nuxt/static/1642688624/blog/on-rss/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642688624",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fon-rss",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b79ab851"}}}}(null));
