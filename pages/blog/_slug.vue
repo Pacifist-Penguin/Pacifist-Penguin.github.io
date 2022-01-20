78a07a1dbabf9e17f9e39473fd0223b6c474de2f
Pacifist-Penguin <7593121+Pacifist-Penguin@users.noreply.github.com>
2022-01-21 00:08:26 +0300

    improved UX for phone users

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -14,23 +14,3 @@ export default Vue.extend({
   }
 })
 </script>
-
-<style>
-.nuxt-content img{
-  display: block;
-  max-width: 65vmin;
-  margin: auto
-}
-
-.nuxt-content h1 {
-  font-size: 2rem
-}
-
-.nuxt-content blockquote {
-  margin-left: 0;
-  margin-right: 0;
-  padding-left: 1rem;
-  background-color: var(--background-dark);
-  border-radius: 0.5rem;
-}
-</style>
