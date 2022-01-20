f5335f78d043a2e0d526f9ee6efec683299df00c
NewPirateOfUASeas <NewPirateOfUASeas@users.noreply.github.com>
2022-01-20 16:54:28 +0000

    deploy: 4496aebd2db2cdcfdf0091eab64b7d52b517394c

--- _nuxt/9fed599.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{338:function(e,n,t){"use strict";t.r(n);var l=t(2).a.extend({name:"IndexPage"}),c=t(41),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("My page")])}),[],!1,null,null,null);n.default=component.exports}}]);
