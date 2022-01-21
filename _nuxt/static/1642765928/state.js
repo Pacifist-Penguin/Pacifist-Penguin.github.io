a5e510c49035d2d09e361ad5b54c1ee51cd55b7f
NewPirateOfUASeas <NewPirateOfUASeas@users.noreply.github.com>
2022-01-21 17:31:52 +0000

    deploy: 91e247a7bbb85b4790625fe8193b3b09d515cdf6

--- _nuxt/static/1642765928/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpersonal-page\u002F_nuxt\u002Fstatic\u002F1642765928",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpersonal-page\u002F",assetsPath:"\u002Fpersonal-page\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"026c4a89"}}}}(null));
---
+++ _nuxt/static/1642786311/project-list/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpersonal-page\u002F_nuxt\u002Fstatic\u002F1642786311",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject-list",config:{_app:{basePath:"\u002Fpersonal-page\u002F",assetsPath:"\u002Fpersonal-page\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62252df2"}}}}(null));
