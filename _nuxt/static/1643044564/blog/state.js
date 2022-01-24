99d6fe23d64bcc4d69570983f94f48f6d67dc3d2
NewPirateOfUASeas <NewPirateOfUASeas@users.noreply.github.com>
2022-01-24 17:16:05 +0000

    deploy: ba57ee5185a7151f3ffc460998bd69965a4f06b4

---
+++ _nuxt/static/1643044564/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpersonal-page\u002F_nuxt\u002Fstatic\u002F1643044564",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002Fpersonal-page\u002F",assetsPath:"\u002Fpersonal-page\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c9eaeade"}}}}(null));
