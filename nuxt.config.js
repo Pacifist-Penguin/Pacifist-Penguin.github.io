0b43d1c4ab27e6cac6b0b0408d4a11d23f59e171
Pacifist-Penguin <7593121+Pacifist-Penguin@users.noreply.github.com>
2022-02-19 20:02:51 +0300

    hopefully fixed favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-      { rel: 'alternate icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
